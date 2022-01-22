2949873eb440b2d74db0c1c51245d15a6be7735e
tushar-amrit-6 <tusharpubg02.com>
2022-01-22 09:30:22 +0530

    Added Bean Classes

---
+++ CRS-JEDI-7-Flipkart-POS/src/com/crs/flipkart/bean/RegisteredStudents.java
@@ -0,0 +1,5 @@
+package com.crs.flipkart.bean;
+
+public class RegisteredStudents {
+
+}
---
+++ CRS-JEDI-7-Flipkart-POS/src/com/crs/flipkart/bean/SemesterRegistration.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.crs.flipkart.bean;
+
+/**
+ * @author JEDI-8
+ *
+ */
+public class SemesterRegistration {
+
+}
