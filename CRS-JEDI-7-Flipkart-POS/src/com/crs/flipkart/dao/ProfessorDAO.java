476f815000a03ed8f358ec447227c7d58c05481d
Vinayak <vinayak.chaturvedi96@gmail.com>
2022-01-24 10:34:15 +0530

    Creating professor Dummy obj

--- CRS-JEDI-7-Flipkart-POS/src/com/crs/flipkart/dao/ProfessorDAO.java
+++ CRS-JEDI-7-Flipkart-POS/src/com/crs/flipkart/dao/ProfessorDAO.java
@@ -2,17 +2,28 @@ package com.crs.flipkart.dao;
 
 import com.crs.flipkart.bean.Professor;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 
 public class ProfessorDAO {
 
     public List<Professor> professorList;
-    Map<Integer, Integer> gradeMap;
+
 
     public void saveProfessor(Professor professor) {
         professorList.add(professor);
     }
+
+    public void createDummy() {
+        professorList.add(new Professor("1", "John", "root", "j@gm.com", 123l,
+                "Professor", "address", "male", 1, "CSE", new ArrayList<>()));
+    }
+
+    public void getProfessorById(int professorId) {
+
+
+    }
+
 
 }
