6519715d0595f9d639a0592ce1e51cbeb76249b6
Vinayak <vinayak.chaturvedi96@gmail.com>
2022-01-25 12:24:10 +0530

    Code Refactor

--- CRS-JEDI-7-Flipkart-POS/src/com/crs/flipkart/dao/ProfessorDAO.java
+++ CRS-JEDI-7-Flipkart-POS/src/com/crs/flipkart/dao/ProfessorDAO.java
@@ -12,13 +12,13 @@ public class ProfessorDAO {
 
     public List<Professor> professorList = new ArrayList<>(
             Arrays.asList(
-                    new Professor("1", "prof1", "root", "p1@gm.com", 78945612, "Professor", "indore", "male", 1, "CSE", new ArrayList<>()),
-                    new Professor("2", "prof2", "root", "p2@gm.com", 98765432, "Professor", "bhopal", "male", 2, "CSE", new ArrayList<>()),
-                    new Professor("3", "prof3", "root", "p3@gm.com", 78945612, "Professor", "indore", "male", 3, "CSE", new ArrayList<>()),
-                    new Professor("4", "prof4", "root", "p4@gm.com", 98765432, "Professor", "bhopal", "male", 4, "CSE", new ArrayList<>()),
-                    new Professor("5", "prof5", "root", "p5@gm.com", 78945612, "Professor", "indore", "male", 5, "CSE", new ArrayList<>()),
-                    new Professor("6", "prof6", "root", "p6@gm.com", 98765432, "Professor", "bhopal", "male", 6, "CSE", new ArrayList<>()),
-                    new Professor("7", "prof7", "root", "p7@gm.com", 78945612, "Professor", "indore", "male", 7, "CSE", new ArrayList<>())
+                    new Professor(1, "prof1", "root", "p1@gm.com", 78945612, "Professor", "indore", "male", "CSE", new ArrayList<>()),
+                    new Professor(2, "prof2", "root", "p2@gm.com", 98765432, "Professor", "bhopal", "male", "CSE", new ArrayList<>()),
+                    new Professor(3, "prof3", "root", "p3@gm.com", 78945612, "Professor", "indore", "male", "CSE", new ArrayList<>()),
+                    new Professor(4, "prof4", "root", "p4@gm.com", 98765432, "Professor", "bhopal", "male", "CSE", new ArrayList<>()),
+                    new Professor(5, "prof5", "root", "p5@gm.com", 78945612, "Professor", "indore", "male", "CSE", new ArrayList<>()),
+                    new Professor(6, "prof6", "root", "p6@gm.com", 98765432, "Professor", "bhopal", "male", "CSE", new ArrayList<>()),
+                    new Professor(7, "prof7", "root", "p7@gm.com", 78945612, "Professor", "indore", "male", "CSE", new ArrayList<>())
             )
     );
 
@@ -34,18 +34,18 @@ public class ProfessorDAO {
         return null;
     }
 
-    public int checkCredentials(String email, String password){
-        for(Professor professor: professorList){
-            if(Objects.equals(professor.getEmailId(), email) && Objects.equals(professor.getPassword(), password))
+    public int checkCredentials(String email, String password) {
+        for (Professor professor : professorList) {
+            if (Objects.equals(professor.getEmailId(), email) && Objects.equals(professor.getPassword(), password))
                 return professor.getProfessorId();
         }
         return -1;
 
     }
 
-    public int updateCredentials(String email, String password){
-        for(Professor professor: professorList){
-            if(Objects.equals(professor.getEmailId(), email))
+    public int updateCredentials(String email, String password) {
+        for (Professor professor : professorList) {
+            if (Objects.equals(professor.getEmailId(), email))
                 professor.setPassword(password);
 
         }
