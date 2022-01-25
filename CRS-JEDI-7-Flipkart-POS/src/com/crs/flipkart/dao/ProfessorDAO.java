6d5a9d2c355e52be6e97f9c85d4f0e776292cc96
Vinayak <vinayak.chaturvedi96@gmail.com>
2022-01-25 15:58:37 +0530

    Creating dao for professor

--- CRS-JEDI-7-Flipkart-POS/src/com/crs/flipkart/dao/ProfessorDAO.java
+++ CRS-JEDI-7-Flipkart-POS/src/com/crs/flipkart/dao/ProfessorDAO.java
@@ -1,14 +1,18 @@
 package com.crs.flipkart.dao;
 
 import com.crs.flipkart.bean.Professor;
+import com.crs.flipkart.utils.DBUtils;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
 
-public class ProfessorDAO implements  ProfessorDaoInterface{
+public class ProfessorDAO implements ProfessorDaoInterface {
 
     public List<Professor> professorList = new ArrayList<>(
             Arrays.asList(
@@ -22,14 +26,70 @@ public class ProfessorDAO implements  ProfessorDaoInterface{
             )
     );
 
-
     public void saveProfessor(Professor professor) {
+        Connection connection = null;
+        PreparedStatement statement = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DBUtils.getConnection();
+            System.out.println("Persisting Profession details");
+            String sqlQuery = "insert into professor values(?, ?, ?, ?, ?, ?, ?, ?)";
+            statement = connection.prepareStatement(sqlQuery);
+            statement.setInt(1, professor.getProfessorId());
+            statement.setString(2, professor.getName());
+            statement.setString(3, professor.getEmailId());
+            statement.setString(4, professor.getPassword());
+            statement.setLong(5, professor.getContactNo());
+            statement.setString(6, professor.getAddress());
+            statement.setString(7, professor.getGender());
+            statement.setString(8, professor.getDepartment());
 
+            statement.executeUpdate();
+
+        } catch (Exception ex) {
+            System.out.println(ex.getLocalizedMessage());
+        } finally {
+            try {
+                if (statement != null) statement.close();
+                if (connection != null) connection.close();
+            } catch (Exception ex) {
+                System.out.println(ex.getLocalizedMessage());
+            }
+        }
     }
 
     public Professor getProfessorById(int professorId) {
-        for (Professor professor : professorList) {
-            if (professor.getProfessorId() == professorId) return professor;
+        Connection connection = null;
+        PreparedStatement statement = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DBUtils.getConnection();
+            System.out.println("Persisting Profession details");
+            String sqlQuery = "select * from professor where professor_id = " + professorId;
+            statement = connection.prepareStatement(sqlQuery);
+            ResultSet resultSet = statement.executeQuery(sqlQuery);
+            Professor professor = new Professor(
+                    resultSet.getInt("professor_id"),
+                    resultSet.getString("name"),
+                    resultSet.getString("password"),
+                    resultSet.getString("email"),
+                    resultSet.getLong("contact_number"),
+                    "Professor",
+                    resultSet.getString("address"),
+                    resultSet.getString("gender"),
+                    resultSet.getString("department"),
+                    new ArrayList<>()
+            );
+            return professor;
+        } catch (Exception ex) {
+            System.out.println(ex.getLocalizedMessage());
+        } finally {
+            try {
+                if (statement != null) statement.close();
+                if (connection != null) connection.close();
+            } catch (Exception ex) {
+                System.out.println(ex.getLocalizedMessage());
+            }
         }
         return null;
     }
